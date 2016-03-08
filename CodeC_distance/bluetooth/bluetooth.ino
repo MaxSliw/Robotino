6d8b152a13ef095c1ba6ac8831990718991d72fa
Ilhat Anthony <tallcraft@outlook.com>
2016-03-08 21:54:29 +0100

    Update bluetooth.ino

--- CodeC_distance/bluetooth/bluetooth.ino
+++ CodeC_distance/bluetooth/bluetooth.ino
@@ -6,37 +6,6 @@
 #define LEDOFF() digitalWrite(PINLED, LOW)
 #define DEBUG_ENABLED 1
 
-<<<<<<< HEAD
-#define RxD         7
-#define TxD         6
-#define DEBUG_ENABLED  1
-#define led 9
-char msg; //ou char
-//+ cs 0,1
-
-SoftwareSerial blueToothSerial(RxD,TxD);
-void setup() {
-  Serial.begin(9600);
-  pinMode(RxD, INPUT);
-  pinMode(TxD, OUTPUT);
-  pinMode(led, OUTPUT);
-  setupBlueToothConnection();
-}
-
-void loop() {
-  char msg;
-  while(1){
-    if(blueToothSerial.available()){
-        msg = blueToothSerial.read();
-        Serial.print(msg);
-        if(msg == '0'){
-          digitalWrite(led, HIGH);
-        }else if(msg == '1'){
-           digitalWrite(led, LOW);
-        }
-        delay(10);
-    }
-=======
 SoftwareSerial blueToothSerial(RxD,TxD);
 void setup(){
   Serial.begin(9600);
@@ -60,29 +29,10 @@ void loop(){
       else if(ordre == 'OFF'){
         LEDOFF();
       }
->>>>>>> 64cb08f675b4a8776c539c55a70560f87fd85166
   }
 }
 }
 void setupBlueToothConnection()
-<<<<<<< HEAD
-{	
-	blueToothSerial.begin(9600);  
-	
-	blueToothSerial.print("AT");
-	delay(400); 
-
-	blueToothSerial.print("AT+DEFAULT"); //Reset
-	delay(2000); 
-	
-	blueToothSerial.print("AT+NAMESeeedBTSlave");  
-	delay(400);
-	
-	
-	blueToothSerial.print("AT+AUTH1"); 
-	delay(400);    
-	blueToothSerial.flush();
-=======
 {
   blueToothSerial.begin(9600);
   blueToothSerial.print("AT");
@@ -96,5 +46,4 @@ void setupBlueToothConnection()
   blueToothSerial.print("AT+AUTH1");
   delay(400);
   blueToothSerial.flush();
->>>>>>> 64cb08f675b4a8776c539c55a70560f87fd85166
 }
