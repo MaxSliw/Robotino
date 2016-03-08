8babf4de679698b9abfd18d5f2c2b7d1d2fa97e7
Ilhat Anthony <tallcraft@outlook.com>
2016-03-08 16:41:28 +0100

    Mise en page

--- CodeC_distance/bluetooth/bluetooth.ino
+++ CodeC_distance/bluetooth/bluetooth.ino
@@ -1,4 +1,4 @@
-#include <SoftwareSerial.h> //Software Serial Port
+#include <SoftwareSerial.h> 
 #define RxD 7
 #define TxD 6
 #define PINLED 9
@@ -8,51 +8,44 @@
 SoftwareSerial blueToothSerial(RxD,TxD);
 void setup()
 {
-Serial.begin(9600);
-pinMode(RxD, INPUT);
-pinMode(TxD, OUTPUT);
-pinMode(PINLED, OUTPUT);
-LEDON();
-setupBlueToothConnection();
+  Serial.begin(9600);
+  pinMode(RxD, INPUT);
+  pinMode(TxD, OUTPUT);
+  pinMode(PINLED, OUTPUT);
+  LEDON();
+  setupBlueToothConnection();
 }
 void loop()
 {
-char recvChar;
-while(1)
+  char recvChar;
+  while(1)
+    {
+    if(blueToothSerial.available())
+    {
+      recvChar = blueToothSerial.read();
+      Serial.print(recvChar);
+      if(recvChar == '1')
+      {
+      LEDON();
+      }
+      else if(recvChar == '0')
+      {
+      LEDOFF();
+      }
+    }
+  }
+}
 {
-if(blueToothSerial.available())
-{//check if there's any data sent from the remote bluetooth shield
-recvChar = blueToothSerial.read();
-Serial.print(recvChar);
-if(recvChar == '1')
-{
-LEDON();
-}
-else if(recvChar == '0')
-{
-LEDOFF();
-}
-}
-}
-}
-/***************************************************************************
-* Function Name: setupBlueToothConnection
-* Description: initilizing bluetooth connction
-* Parameters:
-* Return:
-***************************************************************************/
-void setupBlueToothConnection()
-{
-blueToothSerial.begin(9600);
-blueToothSerial.print("AT");
-delay(400);
-blueToothSerial.print("AT+DEFAULT"); // Restore all setup value to factory setup
-delay(2000);
-blueToothSerial.print("AT+NAMESeeedBTSlave"); // set the bluetooth name as "SeeedBTSlave" ,the length of bluetooth name must less than 12 characters.
-delay(400);
-blueToothSerial.print("AT+PIN0000"); // set the pair code to connect
-delay(400);
-blueToothSerial.print("AT+AUTH1"); //
-delay(400);
-blueToothSerial.flush();
+  blueToothSerial.begin(9600);
+  blueToothSerial.print("AT");
+  delay(400);
+  blueToothSerial.print("AT+DEFAULT"); // Restore all setup value to factory setup
+  delay(2000);
+  blueToothSerial.print("AT+NAMESeeedBTSlave"); // set the bluetooth name as "SeeedBTSlave" ,the length of bluetooth name must less than 12 characters.
+  delay(400);
+  blueToothSerial.print("AT+PIN0000"); // set the pair code to connect
+  delay(400);
+  blueToothSerial.print("AT+AUTH1"); //
+  delay(400);
+  blueToothSerial.flush();
 }
