2c9a97d015c8f11b23bd755a2dcdba1795a5fec1
MaxSliw <anthony.progi@gmail.com>
2016-03-18 21:40:50 +0100

    Test de rapidité du Bluetooth
    Augmentation du nombre de bauds du moniteur ainsi que de la liaison série, passage de char à String sans POO

--- src/main.ino
+++ src/main.ino
@@ -13,7 +13,7 @@ Projet Robotino
 
 SoftwareSerial blueToothSerial(RxD,TxD);
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(RxD, INPUT);
   pinMode(TxD, OUTPUT);
   pinMode(PINLED, OUTPUT);
@@ -21,13 +21,20 @@ void setup(){
   setupBlueToothConnection();
 }
 void loop(){
-  String msg;
+  char msg;
+  String st_msg = "";
   while(1)
   {
     if(blueToothSerial.available()){
-      msg = blueToothSerial.readString();
-      Serial.println(msg);
-      if(msg.substring(0,1) == "1"){
+      msg = blueToothSerial.read();
+      if(msg != '\n'){
+        st_msg += msg;
+        if(st_msg.length() >= 4){
+          Serial.println(st_msg);
+          st_msg = "";
+        }
+      }
+      /*if(msg.substring(0,1) == "1"){
         if(msg.substring(1,2)== "9"){
           if(msg.substring(2,3) == "1"){
             LEDON();
@@ -35,24 +42,37 @@ void loop(){
             LEDOFF();
           }
         }
-      }
-      msg = "";
+      }*/
     }
   }
 }
-
 void setupBlueToothConnection()
 {
-  blueToothSerial.begin(9600);
-  blueToothSerial.print("AT");
-  delay(400);
-  blueToothSerial.print("AT+DEFAULT"); // Restore all setup value to factory setup
-  delay(2000);
-  blueToothSerial.print("AT+NAMERobotino"); // set the bluetooth name as "SeeedBTSlave" ,the length of bluetooth name must less than 12 characters.
-  delay(400);
-  blueToothSerial.print("AT+PIN0000"); //Changement du code pin
-  delay(400);
-  blueToothSerial.print("AT+AUTH1");
-  delay(400);
-  blueToothSerial.flush();
+  /*if(option == "reset"){
+    blueToothSerial.print("AT");
+    delay(400);
+    blueToothSerial.print("AT+DEFAULT"); // Restore all setup value to factory setup
+    delay(400);
+    blueToothSerial.print("AT+UART=115200,1,2");
+    delay(2000);
+    blueToothSerial.print("AT+NAMERobotino"); // set the bluetooth name as "SeeedBTSlave" ,the length of bluetooth name must less than 12 characters.
+    delay(400);
+    blueToothSerial.print("AT+PIN0000"); //Changement du code pin
+    delay(400);
+    blueToothSerial.print("AT+AUTH1");
+    delay(400);
+    blueToothSerial.flush();
+  }else {*/
+    blueToothSerial.begin(9600);
+    blueToothSerial.print("AT");
+    delay(400);
+    blueToothSerial.print("AT+DEFAULT");
+    delay(2000);
+    blueToothSerial.print("AT+UART115200,1,0");
+    //blueToothSerial.print("AT+BAUD8");
+    delay(400);
+    //blueToothSerial.begin(115200);
+    delay(2000);
+    blueToothSerial.flush();
+
 }
