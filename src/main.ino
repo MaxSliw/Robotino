9082c987182b52ec9031e426c7227ba5c6d123ab
MaxSliw <anthony.progi@gmail.com>
2016-03-20 13:10:47 +0100

    Retour à 9600bauds pour BT test réception

--- src/main.ino
+++ src/main.ino
@@ -29,7 +29,7 @@ void loop(){
       msg = blueToothSerial.read();
       if(msg != '\n'){
         st_msg += msg;
-        if(st_msg.length() >= 4){
+        if(st_msg.length() >= 5 && st_msg.substring(0,2) == "18"){
           Serial.println(st_msg);
           st_msg = "";
         }
@@ -48,31 +48,18 @@ void loop(){
 }
 void setupBlueToothConnection()
 {
-  /*if(option == "reset"){
-    blueToothSerial.print("AT");
-    delay(400);
-    blueToothSerial.print("AT+DEFAULT"); // Restore all setup value to factory setup
-    delay(400);
-    blueToothSerial.print("AT+UART=115200,1,2");
-    delay(2000);
-    blueToothSerial.print("AT+NAMERobotino"); // set the bluetooth name as "SeeedBTSlave" ,the length of bluetooth name must less than 12 characters.
-    delay(400);
-    blueToothSerial.print("AT+PIN0000"); //Changement du code pin
-    delay(400);
-    blueToothSerial.print("AT+AUTH1");
-    delay(400);
-    blueToothSerial.flush();
-  }else {*/
     blueToothSerial.begin(9600);
     blueToothSerial.print("AT");
     delay(400);
     blueToothSerial.print("AT+DEFAULT");
     delay(2000);
+    blueToothSerial.print("AT+NAMERobotino");
+    delay(400);
     blueToothSerial.print("AT+UART115200,1,0");
+    delay(400);
+    blueToothSerial.print("AT+AUTH1");
     //blueToothSerial.print("AT+BAUD8");
     delay(400);
-    //blueToothSerial.begin(115200);
-    delay(2000);
     blueToothSerial.flush();
 
 }
