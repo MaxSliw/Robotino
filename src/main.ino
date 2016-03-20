e785929c7260c29bbb1a7fc74bf2246fe438aa10
MaxSliw <anthony.progi@gmail.com>
2016-03-20 16:30:38 +0100

    Convert char to string via array test

--- src/main.ino
+++ src/main.ino
@@ -21,27 +21,17 @@ void setup(){
   setupBlueToothConnection();
 }
 void loop(){
-  char msg;
-  String st_msg = "";
+  char msg[20];
   while(1)
   {
     if(blueToothSerial.available()){
-      msg = blueToothSerial.read();
-      if(msg != '\n'){
-        st_msg += msg;
-        if(st_msg.length() >= 5 && st_msg.substring(0,2) == "18"){
-          Serial.println(st_msg);
-          st_msg = "";
-        }
+      for(int i = 0; i <= 20; i++){
+        msg[i] = blueToothSerial.read();
       }
-      /*if(msg.substring(0,1) == "1"){
-        if(msg.substring(1,2)== "9"){
-          if(msg.substring(2,3) == "1"){
-            LEDON();
-          }else {
-            LEDOFF();
-          }
-        }
+      Serial.println(msg);
+      msg = '\n';
+      /*if(msg.length() >= 5 && msg.substring(0,1) == "1"){
+
       }*/
     }
   }
